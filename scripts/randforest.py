27112971e4c28393aa37298ef12c6550a7b6bf10
Francesca Giordano <fgiordano.data@gmail.com>
2017-12-01 10:21:02 +0000

    Implementing random forest

--- scripts/randforest.py
+++ scripts/randforest.py
@@ -11,16 +11,17 @@ from matplotlib import pyplot as plt
 import fileinput
 
 def get_data(file):
-    df = pd.read_csv(file, sep=" ")
+    df = pd.read_csv(file, sep=" ", header=None)
     data = df.as_matrix()
     np.random.shuffle(data)
-    X = data[:, 1:]  # 100 # norm by bin
-    Y = data[:, 0]
-    Z = data[:, 2] 
+    x = data[:, 1:]  # 100 # norm by bin
+    y = data[:, 0]
+    z = data[:, 1] 
 
-    return X, Z, Y
+    return x, y, z
 
-def find_best():
+
+def find_best(x, y):
     clf =  RandomForestClassifier(n_jobs=-1,max_features= 'sqrt' , n_estimators=50, oob_score = True) 
     param_grid = {        
         'n_estimators': [200, 700, 1000],
@@ -31,12 +32,11 @@ def find_best():
     
     # Create a classifier with the parameter candidates
     CV_rfc = GridSearchCV(estimator=clf, param_grid=param_grid, cv= 5)
-    
     # Train the classifier on training data
-    CV_rfc.fit(X_train, y_train)
+    CV_rfc.fit(x, y)
 
-    print('\n',CV_rfc.best_params_)
-    
+    # Print n return best parameters
+    print('\n',CV_rfc.best_params_)   
     return CV_rfc.best_estimator_.n_estimators, CV_rfc.best_estimator_.max_features, CV_rfc.best_estimator_.criterion 
 
 
@@ -47,57 +47,69 @@ def myfit(x, y):
     return(clf)
 
 
-def plottami(x, y):
-    
 
+def plottami(x, y, z):
+
+    same_chr = np.where(y == 1)[0]
+    print("  I have", len(y), "cases, of which", 
+          same_chr[0], "are from the same chromosome (", same_chr[0]*100./len(y), "%)")
     
-    #plt.hist(Y) 
+    #plt.hist(z) 
     #plt.show()
+    plt.scatter(z,y) 
+    plt.show()
 
 #### main ####
 resize=1
 findbest=0
-fitX=1
+fitX=0
 fitZ=1
-predictX=1
+predictX=0
 predictZ=1
-plot=0
+info=0
 
+
+
+
+### Get Data ###
 
 try: 
     size
 except NameError:    
     myfile='/lustre/scratch117/sciops/team117/hpag/fg6/analysis/Devel/mouse/bothhic_fromscratch/bwa_temp/hicana/map_n_reads.txt'
-    X, Z, target = get_data(myfile)
+    X, target, Z = get_data(myfile)
     size=len(target)
 
 if resize:  
-    X_train, X_test, y_train, y_test = train_test_split(X, target, random_state=1, test_size=0.2)
-    Z=Z.reshape(-1, 1) 
-    Z_train, Z_test, y_train, y_test = train_test_split(Z, target, random_state=1, test_size=0.2)
+    test_perc=0.2
+    X_train, X_test, y_train, y_test = train_test_split(X, target, random_state=1, test_size = test_perc)
+    Z1=Z.reshape(-1, 1) 
+    Z_train, Z_test, y_train, y_test = train_test_split(Z1, target, random_state=1, test_size = test_perc)
 
 
 
 
-if plot:
-    plottami(X, target)
+### Print and Plot some Infos ###
 
+if info:
+    plottami(X, target, Z)
+
+
+
+### Fit n Predict ###
 
 if findbest:
-    n_est, maxf, crit, min_s_leaf = find_best()
+    n_est, maxf, crit, min_s_leaf = find_best(X_train, y_train)
 else:
     n_est = 200 
     maxf = 'auto'
     crit = 'gini'
     min_s_leaf = 10
 
-
 if fitX:    
     Xclf =  myfit(X_train, y_train)
 if fitZ:    
-    Zclf =  myfit(Z_train, y_train)
-
-    
+    Zclf =  myfit(Z_train, y_train)  
    
 if predictX:
     results = Xclf.predict(X_train) 
@@ -107,7 +119,6 @@ if predictX:
     results = Xclf.predict(X_test)
     score =  metrics.accuracy_score(y_test,results)
     print("Using X: test",score)
-
 
 if predictZ:
     results = Zclf.predict(Z_train) 
